b86f1320493be583cff2c7684b03c484fca5f767
gtcanary <3127306807@qq.com>
2025-06-28 16:35:27 +0800

    Initial commit

---
+++ src/main/java/org/example/awa/emm/Emm.java
@@ -0,0 +1,17 @@
+package org.example.awa.emm;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Emm extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info("插件启动成功！");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
